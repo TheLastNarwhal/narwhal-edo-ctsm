02c6166ec4bc0c372dcf2c10792a736c99543962
TheLastNarwhal <92389638+TheLastNarwhal@users.noreply.github.com>
2022-05-11 00:05:33 -0500

    Change to Sirenity Parthenope

--- script/unofficial/c465375.lua
+++ script/unofficial/c465375.lua
@@ -52,21 +52,25 @@ function s.filter(c)
 end
 function s.postg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
   if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc) end
-  if chk==0 then return Duel.IsExistingTarget(s.filter,tp,0,LOCATION_MZONE,1,nil) end
+  if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_MZONE,1,nil) end
   Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
-  local g=Duel.SelectTarget(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil)
+  local g=Duel.GetMatchingGroup(s.filter,tp,0,LOCATION_MZONE,1,1,nil)
   Duel.SetOperationInfo(0,CATEGORY_POSITION,g,#g,0,0)
 end
 function s.posop(e,tp,eg,ep,ev,re,r,rp)
-  local tc=Duel.GetFirstTarget()
-  if tc:IsRelateToEffect(e) and tc:IsDefensePos() and Duel.ChangePosition(tc,0,0,POS_FACEUP_ATTACK,POS_FACEUP_ATTACK,true) then
-    --Cannot change its battle position
-    local e1=Effect.CreateEffect(e:GetHandler())
-    e1:SetDescription(3313)
-    e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
-    e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
-    e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-    tc:RegisterEffect(e1)
+  local g=Duel.SelectMatchingCard(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil)
+  if #g>0 then
+    Duel.ChangePosition(g,0,0,POS_FACEUP_ATTACK,POS_FACEUP_ATTACK,true)
+    local tc=g:GetFirst()
+      if tc:IsAttackPos() then
+      --Cannot change its battle position
+      local e1=Effect.CreateEffect(e:GetHandler())
+      e1:SetDescription(3313)
+      e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+      e1:SetType(EFFECT_TYPE_SINGLE)
+      e1:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
+      e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+      tc:RegisterEffect(e1)
+    end
   end
 end
